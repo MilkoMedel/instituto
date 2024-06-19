89d92afbb5ad1955d0196ecb95f592b9397eec51
MilkoMedel <m.ignaciomedel@gmail.com>
2024-06-18 21:47:40 -0400

    cambios a settings
    se corrigen cambios en settings.py

--- Proyecto/instituto/ventas/views.py
+++ Proyecto/instituto/ventas/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     return render(request, 'ventas/index.html')
 
 def login(request):
     return render(request, 'ventas/login.html')
-
+@login_required
 def galeria(request):
     return render(request, 'ventas/galeria.html')
-
+@login_required
 def nosotros(request):
     return render(request, 'ventas/nosotros.html')
 
